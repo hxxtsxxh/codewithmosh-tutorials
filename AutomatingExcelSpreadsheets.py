2f611698794870b9b9cd5507165c33c9fb591531
Heet Shah <heet1356@gmail.com>
2021-05-14 19:03:19 -0400

    Update AutomatingExcelSpreadsheets.py

--- AutomatingExcelSpreadsheets.py
+++ AutomatingExcelSpreadsheets.py
@@ -1,4 +1,6 @@
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
+
 wb = xl.load_workbook('testXML.xlsx') # loads the xml
 sheet = wb['Sheet1'] #accesses the sheet
 cell = sheet['a1']
@@ -9,6 +11,16 @@ for row in range(2, sheet.max_row + 1):
    corrected_price = cell.value * 0.9
    corrected_price_cell = sheet.cell(row, 4)
    corrected_price_cell.value = corrected_price
+
+values = Reference(sheet,
+          min_row=2,
+          max_row=sheet.max_row,
+          min_col=4,
+          max_col=4)
+
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, 'e2')
 
 wb.save('test2XML.xlsx')
 
