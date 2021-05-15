a62683f13591aa38d724f9f6f76b4b9a5e835a49
Heet Shah <heet1356@gmail.com>
2021-05-14 20:08:14 -0400

    Updated

--- ModulesImport.py
+++ ModulesImport.py
@@ -11,7 +11,9 @@
 from modules.utils import find_max
 
 numbers = [0, 5, 6, 13, 4, 6, 8, 10]
+numbers2 = [1, 4, 2, 4, 5, 3, 8]
 
 result = find_max(numbers)
+result2 = find_max(numbers2)
 
 print(result)
